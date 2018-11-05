143e9e42b88757e80d4057258636f5d1c581dece
tugenhua <tugenhua@tugenhuadeMacBook-Pro.local>
2018-11-05 11:59:22 +0800

    update

---
+++ webpack+vue+mongodb+express/app/index/views/list.vue
@@ -0,0 +1,47 @@
+<style lang="stylus">
+  #list-container 
+    width 100%
+</style>
+<template>
+  <div id="list-container">
+    <el-form ref="form" label-width="80px">
+      <el-form-item label="姓名">
+        <el-input v-model="name"></el-input>
+      </el-form-item>
+      <el-form-item label="年龄">
+        <el-input v-model="age"></el-input>
+      </el-form-item>
+      <el-form-item label="性别">
+        <el-select v-model="sex">
+          <el-option
+            v-for="item in options2"
+            :key="item.value"
+            :label="item.label"
+            :value="item.value">
+          </el-option>
+        </el-select>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+
+<script type="text/javascript">
+  export default {
+    data() {
+      return {
+        name: '',
+        age: '',
+        sex: '',
+        options2: [
+          {
+            value: '1',
+            label: '男'
+          }, {
+            value: '2',
+            label: '女'
+          }
+        ]
+      }
+    }
+  }
+</script>
